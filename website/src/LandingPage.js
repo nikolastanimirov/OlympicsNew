c4a15158c7ca162c4d38919479167975f26d79ca
Nick <nikeca17_kn@abv.bg>
2020-06-03 13:46:08 +0300

    fixed3

--- website/src/LandingPage.js
+++ website/src/LandingPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./static/App.css";
 import Header from "./sources/header";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./sources/Home";
 import Contact from "./sources/Contact";
 import Footer from "./sources/Footer";
